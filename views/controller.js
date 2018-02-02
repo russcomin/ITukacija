ef7821feb89157875a7236638085e746f909bbb8
unknown <jovanbelic234@gmail.com>
2018-02-02 22:58:07 +0100

    menu-settings

--- views/controller.js
+++ views/controller.js
@@ -3,3 +3,24 @@ let image_div = document.getElementById('image-holder');
 
 let name = document.getElementById('Kname');
 	name.innerHTML = localStorage['username'];
+
+let arrow = document.getElementById('arrow');
+let menu = document.getElementById('menu')
+	arrow.addEventListener('click', openMenu);
+
+let i = 0;
+
+	function openMenu() {
+		menu.style.display = "block";
+		arrow.style.transform = "rotate(180deg)";
+		i++;
+		if (i == 2) {
+			closeMenu();
+			i=0;
+		}
+	}
+
+	function closeMenu(){
+		menu.style.display = "none";
+		arrow.style.transform = "rotate(0deg)";
+	}
